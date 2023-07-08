ccd37ff330af3b8a90e2321bf2d526699ebe8f1c
Kadu Plentis <83604920+Kadu1811@users.noreply.github.com>
2023-07-08 18:30:46 -0300

    chore: install image and improve styles, add image folder

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,40 +1,48 @@
-import { Link, Route, useHref } from 'react-router-dom'
+import { Image } from 'antd'
 import {
   ButtonArea,
   ButtonLearn,
   ButtonShop,
+  Container,
   Description,
-  HomeContainer,
   ImageContainer,
   TextArea,
-  TextContainer,
   Title,
 } from './styles'
 
 const Home = () => {
   return (
     <>
-      <HomeContainer>
-        <TextContainer>
-          {/* <div style={{ background: "blue" }}>Imagem</div> */}
+      <Container>
+        {/* <div style={{ background: "blue" }}>Imagem</div> */}
 
-          <TextArea>
-            <Title>
-              BOOST <br /> YOUR STYLE <br /> SENSE
-            </Title>
-            <Description>
-              Lorem Ipsum is simply dummy text of the printing and typesetting industry.
-            </Description>
+        <TextArea>
+          <Title>
+            BOOST <br /> YOUR STYLE <br /> SENSE
+          </Title>
 
-            <ButtonArea>
-              <ButtonShop href="/">Shop Now</ButtonShop>
-              <ButtonLearn href="/">Learn More</ButtonLearn>
-            </ButtonArea>
-          </TextArea>
-        </TextContainer>
+          <Description>
+            Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum
+            is simply dummy.
+          </Description>
 
-        <ImageContainer>oi</ImageContainer>
-      </HomeContainer>
+          <ButtonArea>
+            <ButtonShop href="/">Shop Now</ButtonShop>
+
+            <ButtonLearn href="/">Learn More</ButtonLearn>
+          </ButtonArea>
+        </TextArea>
+
+        <ImageContainer>
+          <Image
+            style={{ borderRadius: '0 0 0 80px', paddingTop: '150px' }}
+            width={950}
+            preview={false}
+            src="https://media.glamourmagazine.co.uk/photos/618d036c23e7c974d9039be4/16:9/w_2560%2Cc_limit/ZENDAYA_111121_GettyImages-1352575427_L.jpg"
+          />
+          {/* <img src={zendaya} alt="" width={200} height={400} /> */}
+        </ImageContainer>
+      </Container>
       <div>oi</div>
     </>
   )
